dd38232d29730548d2fc4548daf5af9d45dff1a1
avp02 <vpetranovskij@gmail.com>
2022-06-25 12:36:21 +0300

    added package repository in product-service module

---
+++ product-service/src/main/java/com/avp/productservice/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.avp.productservice.repository;
+
+import com.avp.productservice.model.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends MongoRepository<Product, String> {
+}
