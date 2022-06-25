e33d10576313c0bd31fffe493d0368ddea05c8eb
avp02 <67960868+avp02@users.noreply.github.com>
2022-06-25 11:01:57 +0300

    Add files via upload

---
+++ src/test/java/com/avp/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avp.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
